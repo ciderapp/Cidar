60b06bbf2615d5c81037453d19ccca3d0c98d077
freehelpdesk <freehelpdesk@proton.me>
2023-07-04 15:17:35 -0400

    cidar: fix time, add conversion counter

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,14 @@ use serenity::framework::StandardFramework;
 use serenity::model::Timestamp;
 use serenity::model::gateway::Ready;
 use serenity::model::application::component::ButtonStyle;
-use serenity::model::prelude::{Message, Embed};
+use serenity::model::prelude::Message;
 use serenity::prelude::*;
 
 use regex::Regex;
-
+use surrealdb::Surreal;
+use surrealdb::engine::remote::ws::{Client, Ws};
+use surrealdb::opt::auth::Root;
+use surrealdb::sql::Thing;
 
 type TokenLock = Arc<RwLock<Option<String>>>;
 
@@ -23,7 +26,7 @@ struct Handler {
     api: AppleMusicApi,
     url_regex: Regex,
     apple_regex: Regex,
-    spotify_regex: Regex
+    spotify_regex: Regex,
 }
 
 struct AppleMusicApi {
@@ -90,7 +93,7 @@ trait Time {
 
 impl Time for u64 {
     fn milli_to_hhmmss(&self) -> String {
-        let seconds = self / 100;
+        let seconds = self / 1000;
         let ss = seconds % 60;
         let mm = (seconds / 60) % 60;
         let hh = (seconds / (60 * 60)) % 24;
@@ -117,11 +120,12 @@ fn wh(url: &str, w: u32, h: u32) -> String {
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _ctx: serenity::prelude::Context, ready: Ready) {
+    async fn ready(&self, ctx: serenity::prelude::Context, ready: Ready) {
         println!("{} is connected", ready.user.name);
+        tokio::task::spawn(status_updater(ctx.clone()));
     }
 
-    async fn message(&self, _ctx: serenity::prelude::Context, _new_message: Message) {
+    async fn message(&self, _ctx: serenity::prelude::Context, mut _new_message: Message) {
         // if _new_message.channel_id.0 != 1125513784384036874 {
         //     return;
         // }
@@ -136,9 +140,6 @@ impl EventHandler for Handler {
                 Some(url) => url.as_str().to_string(),
                 None => return,
             };
-
-            let orig_url = url.clone();
-
 
             // Check to see it it matches either one of our regular expressions
             if !self.apple_regex.is_match(&url) && !self.spotify_regex.is_match(&url) {
@@ -186,29 +187,38 @@ impl EventHandler for Handler {
             let storefront: Vec<&str> = longer.split('/').collect();
             let storefront = &storefront[1];
 
-            let mut content = _new_message.content.replace(&orig_url, "[embed]");
-            if content.trim() == "[embed]" {
-                content = String::from("")
-            }
 
             let mut description: String = Default::default();
             let mut duration: Duration = Default::default();
 
             let mut resp: Value = Default::default();
 
-            if url.contains("songs") || query.contains_key("i") {
+            let mut media = Media::default();
+
+            if url.contains("song") || query.contains_key("i") {
                 let id = match query.get("i") {
                     Some(i) => i,
-                    None => return,    
+                    None => {
+                        parsed_url.path_segments().unwrap().last().unwrap()
+                    },    
                 };
-    
-                println!("{}", id);
 
                 resp = self.api.request_endpoint(Method::GET, &format!("v1/catalog/{}/songs/{}", storefront, &id)).await;
+                
+                media.sid = id.to_string();
+                media.media_type = MediaType::Song;
+
+                let name = resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap().to_string();
+                media.name = name;
+
                 description = format!("Listen to {} by {} on Cider", resp.get_value_by_path("data.0.attributes.albumName").unwrap().as_str().unwrap(), resp.get_value_by_path("data.0.attributes.artistName").unwrap().as_str().unwrap());
                 duration = Duration::from_millis(resp.get_value_by_path("data.0.attributes.durationInMillis").unwrap().as_u64().unwrap_or(0));
             } else if url.contains("album") {
-                resp = self.api.request_endpoint(Method::GET, &format!("v1/catalog/{}/albums/{}", storefront, parsed_url.path_segments().unwrap().last().unwrap())).await;
+                let id = parsed_url.path_segments().unwrap().last().unwrap();
+                resp = self.api.request_endpoint(Method::GET, &format!("v1/catalog/{}/albums/{}", storefront, id)).await;
+
+                media.sid = id.to_string();
+                media.media_type = MediaType::Album;
 
                 let mut total_duration: u64 = 0;
 
@@ -216,10 +226,17 @@ impl EventHandler for Handler {
                     total_duration += resp.get_value_by_path(&format!("data.0.relationships.tracks.data.{i}.attributes.durationInMillis")).unwrap().as_u64().unwrap();
                 }
 
-                description = format!("Listen to {} by {} on Cider", resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap(), resp.get_value_by_path("data.0.attributes.artistName").unwrap().as_str().unwrap());
+                let name = resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap().to_string();
+                media.name = name.clone();
+
+                description = format!("Listen to {} by {} on Cider", name, resp.get_value_by_path("data.0.attributes.artistName").unwrap().as_str().unwrap());
                 duration = Duration::from_millis(total_duration);
             } else if url.contains("playlist") {
-                resp = self.api.request_endpoint(Method::GET, &format!("v1/catalog/{}/playlists/{}", storefront, parsed_url.path_segments().unwrap().last().unwrap())).await;
+                let id = parsed_url.path_segments().unwrap().last().unwrap();
+                resp = self.api.request_endpoint(Method::GET, &format!("v1/catalog/{}/playlists/{}", storefront, id)).await;
+
+                media.sid = id.to_string();
+                media.media_type = MediaType::Playlist;
 
                 let mut total_duration: u64 = 0;
 
@@ -227,15 +244,35 @@ impl EventHandler for Handler {
                     total_duration += resp.get_value_by_path(&format!("data.0.relationships.tracks.data.{i}.attributes.durationInMillis")).unwrap().as_u64().unwrap();
                 }
 
-                description = format!("Listen to {} by {} on Cider", resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap(), resp.get_value_by_path("data.0.attributes.curatorName").unwrap().as_str().unwrap());
+                let name = resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap().to_string();
+                media.name = name.clone();
+
+                description = format!("Listen to {} by {} on Cider", name, resp.get_value_by_path("data.0.attributes.curatorName").unwrap().as_str().unwrap());
                 duration = Duration::from_millis(total_duration);
             } else if url.contains("music-video") {
-                resp = self.api.request_endpoint(Method::GET, &format!("v1/catalog/{}/music-video/{}", storefront, parsed_url.path_segments().unwrap().last().unwrap())).await;
-                description = format!("Listen to {} by {} on Cider", resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap(), resp.get_value_by_path("data.0.attributes.artistName").unwrap().as_str().unwrap());
+                let id = parsed_url.path_segments().unwrap().last().unwrap();
+                resp = self.api.request_endpoint(Method::GET, &format!("v1/catalog/{}/music-video/{}", storefront, id)).await;
+                
+                media.sid = id.to_string();
+                media.media_type = MediaType::MusicVideo;
+
+                let name = resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap().to_string();
+                media.name = name.clone();
+
+                description = format!("Listen to {} by {} on Cider", name, resp.get_value_by_path("data.0.attributes.artistName").unwrap().as_str().unwrap());
                 duration = Duration::from_millis(resp.get_value_by_path("data.0.attributes.durationInMillis").unwrap().as_u64().unwrap_or(0));
             } else if url.contains("artist") {
-                resp = self.api.request_endpoint(Method::GET, &format!("v1/catalog/{}/artists/{}", storefront, parsed_url.path_segments().unwrap().last().unwrap())).await;
-                description = format!("Listen to {} on Cider", resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap());
+                let id = parsed_url.path_segments().unwrap().last().unwrap();
+                resp = self.api.request_endpoint(Method::GET, &format!("v1/catalog/{}/artists/{}", storefront, id)).await;
+                
+                media.sid = id.to_string();
+                media.media_type = MediaType::Artist;
+
+                let name = resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap().to_string();
+                media.name = name.clone();
+                description = format!("Listen to {} on Cider", name);
+            } else {
+                return;
             }
 
             // So if we create an embed each time, it takes 3 entire seconds per the call of the next three lines
@@ -245,35 +282,37 @@ impl EventHandler for Handler {
             // webhook.edit_name(&_ctx.http, &_new_message.author.name).await.unwrap();
 
             // Speed up from 4 seconds to just 0.70 ish
-            let mut webhook = match _new_message.channel_id.webhooks(&_ctx.http).await {
-                Ok(hooks) => {
-                    let mut iterator = hooks.iter();
-                    // tomfoolery
-                    if let Some(webhook) = iterator.find(|&hook| {
-                        //println!("bot: {}, http: {}", &hook.user.as_ref().unwrap().id.0, &_ctx.cache.current_user().id.0);
-                        &hook.user.as_ref().unwrap().id.0 == &_ctx.cache.current_user().id.0
-                    }) {
-                        webhook.clone()
-                    } else {
-                        _new_message.channel_id.create_webhook(&_ctx.http, "cidar-webhook").await.unwrap()
-                    }
-                },
-                Err(_) => {
-                    // We actually dont have any webhooks in the channel, so add cidars one.
-                    _new_message.channel_id.create_webhook(&_ctx.http, "cidar-webhook").await.unwrap()
-                }
-            };
 
-            webhook.edit_avatar(&_ctx.http, &*_new_message.author.avatar_url().unwrap()).await.unwrap();
-            webhook.edit_name(&_ctx.http, &_new_message.author.name).await.unwrap();
+            // let mut webhook = match _new_message.channel_id.webhooks(&_ctx.http).await {
+            //     Ok(hooks) => {
+            //         let mut iterator = hooks.iter();
+            //         // tomfoolery
+            //         if let Some(webhook) = iterator.find(|&hook| {
+            //             //println!("bot: {}, http: {}", &hook.user.as_ref().unwrap().id.0, &_ctx.cache.current_user().id.0);
+            //             &hook.user.as_ref().unwrap().id.0 == &_ctx.cache.current_user().id.0
+            //         }) {
+            //             webhook.clone()
+            //         } else {
+            //             _new_message.channel_id.create_webhook(&_ctx.http, "cidar-webhook").await.unwrap()
+            //         }
+            //     },
+            //     Err(_) => {
+            //         // We actually dont have any webhooks in the channel, so add cidars one.
+            //         _new_message.channel_id.create_webhook(&_ctx.http, "cidar-webhook").await.unwrap()
+            //     }
+            // };
+
+            // webhook.edit_avatar(&_ctx.http, &*_new_message.author.avatar_url().unwrap()).await.unwrap();
+            // webhook.edit_name(&_ctx.http, &_new_message.author.name).await.unwrap();
 
             let modded = url.replace("https://", "");
 
             let play_link = format!("https://cider.sh/p?{}", modded);
             let view_link = format!("https://cider.sh/o?{}", modded);
 
-            let embed = Embed::fake(|e| {
-                e.title(resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap_or("N/A"))
+            _new_message.channel_id.send_message(&_ctx.http, |m| {
+                m.embed(|e| {
+                    e.title(resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap_or("N/A"))
                     .url(resp.get_value_by_path("data.0.attributes.url").unwrap().as_str().unwrap_or("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
                     .thumbnail(wh(resp.get_value_by_path("data.0.attributes.artwork.url").unwrap().as_str().unwrap(), 512, 512))
                     .description(&description)
@@ -281,10 +320,7 @@ impl EventHandler for Handler {
                         f.text(format!("Shared by {} | {} • {}", _new_message.author.name, duration.milli_to_hhmmss(), resp.get_value_by_path("data.0.attributes.releaseDate").unwrap_or(Value::String("".to_string())).as_str().unwrap()))
                     })
                     .timestamp(Timestamp::now())
-            });
-
-            webhook.execute(&_ctx.http, false, |m| {
-                m.content(content).embeds(vec![embed.clone()]).components(|c| {
+                }).components(|c| {
                     c.create_action_row(|r| {
                         r.create_button(|b| {
                             b.label("Play in Cider")
@@ -300,22 +336,132 @@ impl EventHandler for Handler {
                 })
             }).await.unwrap();
 
-            webhook.edit_name(&_ctx.http, "cidar-webhook").await.unwrap();
-            webhook.delete_avatar(&_ctx.http).await.unwrap();
+            // let embed = Embed::fake(|e| {
+            //     e.title(resp.get_value_by_path("data.0.attributes.name").unwrap().as_str().unwrap_or("N/A"))
+            //         .url(resp.get_value_by_path("data.0.attributes.url").unwrap().as_str().unwrap_or("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
+            //         .thumbnail(wh(resp.get_value_by_path("data.0.attributes.artwork.url").unwrap().as_str().unwrap(), 512, 512))
+            //         .description(&description)
+            //         .footer(|f| {
+            //             f.text(format!("Shared by {} | {} • {}", _new_message.author.name, duration.milli_to_hhmmss(), resp.get_value_by_path("data.0.attributes.releaseDate").unwrap_or(Value::String("".to_string())).as_str().unwrap()))
+            //         })
+            //         .timestamp(Timestamp::now())
+            // });
 
-            match _new_message.delete(&_ctx.http).await {
-                Ok(_) => (),
-                Err(_) => return,
+            // webhook.execute(&_ctx.http, false, |m| {
+            //     m.content(content).embeds(vec![embed.clone()]).components(|c| {
+            //         c.create_action_row(|r| {
+            //             r.create_button(|b| {
+            //                 b.label("Play in Cider")
+            //                 .style(ButtonStyle::Link)
+            //                 .url(play_link)
+            //             })
+            //             .create_button(|b| {
+            //                 b.label("View in Cider")
+            //                 .style(ButtonStyle::Link)
+            //                 .url(view_link)
+            //             })
+            //         })
+            //     })
+            // }).await.unwrap();
+
+            // webhook.edit_name(&_ctx.http, "cidar-webhook").await.unwrap();
+            // webhook.delete_avatar(&_ctx.http).await.unwrap();
+
+            // match _new_message.delete(&_ctx.http).await {
+            //     Ok(_) => (),
+            //     Err(_) => {
+            //         println!("failed to delete message");
+            //         return
+            //     },
+            // };
+
+            _new_message.suppress_embeds(&_ctx.http).await.unwrap();
+
+            // Update the conversions
+            let mut read_store: Store = match DB.select(("stats", "conversions")).await {
+                Ok(s) => s,
+                Err(_) => {
+                    let s: Store = DB.create(("stats", "conversions"))
+                        .content(Store::default()).await.unwrap();
+                    s
+                }
             };
+            read_store.total_conversions += 1;
+
+            let _: Store = DB.update(("stats", "conversions"))
+            .content(read_store).await.unwrap();
+
+            // Update user information
+            let user = User { 
+                username: _new_message.author.name,
+                ..Default::default()
+            };
+
+            let _: User = DB.update(("users", _new_message.author.id.0))
+                .content(user).await.unwrap();
+
+            let parsed_id = media.sid.parse::<u64>().unwrap();
+
+            let _: Media = DB.update(("media", parsed_id))
+                .content(media).await.unwrap();
+
+            // For some reason, binding does not work.
+            DB.query(&format!("RELATE users:{}->conversions->media:{}",_new_message.author.id.0, parsed_id))
+                .await.unwrap();
         }
     }
 }
 
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Store {
+    total_conversions: u64,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct User {
+    id: Option<Thing>,
+    username: String,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+enum MediaType {
+    #[default]
+    Song,
+    Album,
+    Playlist,
+    MusicVideo,
+    Artist
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Media {
+    #[serde(skip_serializing)]
+    id: Option<Thing>,
+    name: String,
+    media_type: MediaType,
+
+    #[serde(skip_serializing, skip_deserializing)]
+    sid: String,
+}
+
+
+static DB: Surreal<Client> = Surreal::init();
+
 #[tokio::main]
 async fn main() {
-    // Get discord token
-    let token = std::env::var("TOKEN").expect("Please set the TOKEN env variable");
     println!("Cidar launching");
+    let token = std::env::var("TOKEN").expect("Please set the TOKEN env variable");
+    let database_ip = std::env::var("DB_IP").expect("Please set the DB_IP env variable");
+    let database_password = std::env::var("DB_PASS").expect("Please set the DB_PASS env variable");
+
+    println!("Connecting to database");
+    DB.connect::<Ws>(database_ip).await.expect("Unable to connect to database");
+    DB.signin(Root {
+        username: "root",
+        password: &database_password,
+    }).await.unwrap();
+
+    DB.use_ns("cider").use_db("cidar").await.unwrap();
 
     let developer_token: TokenLock = Default::default(); // We need this smart pointer to give to the thread that handles token updates
 
@@ -337,7 +483,7 @@ async fn main() {
         spotify_regex: Regex::new(r"open.spotify.com/(.+[a-z](/?)+)").unwrap(),
     };
 
-    let mut client = Client::builder(token, intents)
+    let mut client = serenity::Client::builder(token, intents)
         .event_handler(handler)
         .framework(StandardFramework::new())
         .await
@@ -370,5 +516,17 @@ async fn token_updater(token: TokenLock) {
         *token.write().await = Some(response.token);
 
         tokio::time::sleep(Duration::from_secs(60 * 30)).await; // Sleep for 30 minutes 
+    }
+}
+
+async fn status_updater(ctx: serenity::prelude::Context) {
+    use serenity::model::gateway::Activity;
+    use serenity::model::user::OnlineStatus;
+    let status = OnlineStatus::DoNotDisturb;
+    loop {
+        let read_store: Store = DB.select(("stats", "conversions")).await.unwrap_or(Store::default());
+        let activity = Activity::listening(format!("Cider | {} songs converted",read_store.total_conversions ));
+        ctx.set_presence(Some(activity.clone()), status).await;
+        tokio::time::sleep(Duration::from_secs(10)).await;
     }
 }
